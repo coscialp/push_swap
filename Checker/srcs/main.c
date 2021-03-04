e7a466033c10a2a76844ee927c6f8fe422b6afd1
coscialp <corentin.scialpi@gmail.com>
2021-03-04 16:31:04 +0100

    checker init

--- Checker/srcs/main.c
+++ Checker/srcs/main.c
@@ -6,13 +6,41 @@
 /*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 13:13:05 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/04 13:22:19 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/04 15:59:35 by coscialp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stack.h"
 
-int	main(void)
+static int	ft_is_number(int c)
 {
+	return (ft_isdigit(c) || c == '-');
+}
+
+int	main(int ac, char **av)
+{
+	static char	*line = NULL;
+	t_stack		stack;
+
+	if (ac >= 2)
+	{
+		stack = new_stack();
+		while (ac-- > 1)
+		{
+			if (ft_stris(av[ac], ft_is_number))
+				stack.push(&stack, ft_atoi(av[ac]));
+			else
+			{
+				ft_dprintf(2, "Error:\tOnly numbers are accepted!\n");
+				return (1);
+			}
+		}
+		while (get_next_line(0, &line) > 0)
+		{
+			ft_dprintf(1, "%s\n", line);
+			ft_strdel(&line);
+		}
+	}
 	return (0);
 }
