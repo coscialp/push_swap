4d052776a0bc875364780f2def075144694e7663
akerdeka <kerdea@yahoo.fr>
2021-03-05 11:50:30 +0100

    rotate

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -6,7 +6,7 @@
 /*   By: akerdeka <akerdeka@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 10:47:20 by akerdeka          #+#    #+#             */
-/*   Updated: 2021/03/05 11:43:41 by akerdeka         ###   ########lyon.fr   */
+/*   Updated: 2021/03/05 11:50:17 by akerdeka         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,24 @@ void	rotate_a(t_push_stack *stack)
 	tmp->_next = stack->stack_a->_data->_prev;
 	stack->stack_a->_data->_prev->_next = NULL;
 	stack->stack_a->_data->_prev = NULL;
-
 }
 
 void	rotate_b(t_push_stack *stack)
-{}
+{
+	t_node_stack	*tmp;
+
+	tmp = stack->stack_b;
+	while (tmp->_next)
+		tmp = tmp->_next;
+	stack->stack_b->_data = stack->stack_b->_data->_next;
+	stack->stack_b->_data->_prev->_prev = tmp;
+	tmp->_next = stack->stack_b->_data->_prev;
+	stack->stack_b->_data->_prev->_next = NULL;
+	stack->stack_b->_data->_prev = NULL;
+}
 
 void	rotate_r(t_push_stack *stack)
-{}
+{
+	rotate_a(stack);
+	rotate_b(stack);
+}
