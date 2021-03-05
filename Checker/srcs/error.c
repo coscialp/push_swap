3365f54b86fd5f1d10e0330d3c98463d6046192a
coscialp <corentin.scialpi@gmail.com>
2021-03-05 09:25:47 +0100

    cleaning

--- Checker/srcs/error.c
+++ Checker/srcs/error.c
@@ -1,41 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/04 13:13:05 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/05 09:11:29 by coscialp         ###   ########lyon.fr   */
+/*   Created: 2021/03/05 09:01:20 by coscialp          #+#    #+#             */
+/*   Updated: 2021/03/05 09:12:02 by coscialp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+void	log_error(int error)
 {
-	static char	*line = NULL;
-	t_stack		stack;
-
-	if (ac >= 2)
-	{
-		stack = new_stack();
-		while (ac-- > 1)
-		{
-			if (ft_stris(av[ac], ft_is_number))
-			{
-				if (stack.push(&stack, ft_atoi(av[ac])) == -1)
-					log_error(DUNUM);
-			}
-			else
-				log_error(NONUM);
-		}
-		while (get_next_line(0, &line) > 0)
-		{
-			ft_dprintf(1, "%s\n", line);
-			ft_strdel(&line);
-		}
-	}
-	return (0);
+	if (error == DUNUM)
+		ft_dprintf(2, "Error:\tNo duplicates numbers!\n");
+	else if (error == NONUM)
+		ft_dprintf(2, "Error:\tOnly numbers are accepted!\n");
+	exit(1);
 }
---
+++ Includes/push_swap.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 09:06:26 by coscialp          #+#    #+#             */
+/*   Updated: 2021/03/05 09:11:52 by coscialp         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../Dependencies/libft/includes/stack.h"
+
+# define DUNUM 0x000001
+# define NONUM 0x000002
+
+void	log_error(int error);
+
+#endif
+
