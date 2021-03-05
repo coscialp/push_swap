0d841b998f92295fd5c359a2e9350ed8bc6e9aa9
akerdeka <kerdea@yahoo.fr>
2021-03-05 11:49:07 +0100

    Push_stack functions

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -6,7 +6,7 @@
 /*   By: akerdeka <akerdeka@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 10:47:20 by akerdeka          #+#    #+#             */
-/*   Updated: 2021/03/05 11:06:07 by akerdeka         ###   ########lyon.fr   */
+/*   Updated: 2021/03/05 11:43:41 by akerdeka         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 
 void	rotate_a(t_push_stack *stack)
 {
-	t_stack	*tmp;
+	t_node_stack	*tmp;
 
+	tmp = stack->stack_a;
+	while (tmp->_next)
+		tmp = tmp->_next;
+	stack->stack_a->_data = stack->stack_a->_data->_next;
+	stack->stack_a->_data->_prev->_prev = tmp;
+	tmp->_next = stack->stack_a->_data->_prev;
 	stack->stack_a->_data->_prev->_next = NULL;
-	
+	stack->stack_a->_data->_prev = NULL;
+
 }
 
 void	rotate_b(t_push_stack *stack)
