1b77f999af87990071cfe6833eaa509293edb3c7
akerdeka <kerdea@yahoo.fr>
2021-03-05 09:58:09 +0100

    struct

--- Includes/push_swap.h
+++ Includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: akerdeka <akerdeka@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 09:06:26 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/05 09:11:52 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/05 09:57:54 by akerdeka         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,35 @@
 
 void	log_error(int error);
 
-#endif
+typedef struct s_push_stack		t_push_stack;
+struct s_push_stack
+{
+	t_stack			*stack_a;
+	t_stack			*stack_b;
+	void			(*sa)(t_push_stack *);
+	void			(*sb)(t_push_stack *);
+	void			(*ss)(t_push_stack *);
+	void			(*pa)(t_push_stack *);
+	void			(*pb)(t_push_stack *);
+	void			(*ra)(t_push_stack *);
+	void			(*rb)(t_push_stack *);
+	void			(*rr)(t_push_stack *);
+	void			(*rra)(t_push_stack *);
+	void			(*rrb)(t_push_stack *);
+	void			(*rrr)(t_push_stack *);
+};
 
+t_push_stack	new_push_stack(void);
+void			swap_a(t_push_stack *stack);
+void			swap_b(t_push_stack *stack);
+void			swap_s(t_push_stack *stack);
+void			push_a(t_push_stack *stack);
+void			push_b(t_push_stack *stack);
+void			rotate_a(t_push_stack *stack);
+void			rotate_b(t_push_stack *stack);
+void			rotate_r(t_push_stack *stack);
+void			r_rotate_a(t_push_stack *stack);
+void			r_rotate_b(t_push_stack *stack);
+void			r_rotate_r(t_push_stack *stack);
+
+#endif
