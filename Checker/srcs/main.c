5c43d0a16efd41adb7d5d5937570c9715c4a1db2
akerdeka <kerdea@yahoo.fr>
2021-03-08 08:43:46 +0100

    norm not finished

--- Checker/srcs/main.c
+++ Checker/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: akerdeka <akerdeka@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 13:13:05 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/05 14:12:42 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/08 08:41:24 by akerdeka         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ int	get_value(char **ptr)
 
 void	check_stack(t_push_stack s)
 {
+	t_node_stack	*a;
+
 	if (s.stack_b->_size != 0)
 	{
 		ft_dprintf(1, "[KO]\n");
 		exit(1);
 	}
-	t_node_stack *a;
-
 	a = s.stack_a->_data;
 	while (a->_next)
 	{
@@ -50,10 +50,37 @@ void	check_stack(t_push_stack s)
 	exit(0);
 }
 
+static void	checker(t_push_stack stack, char *ptr, t_instruc *insn, int stop)
+{
+	int			value;
+	short		i;
+	static char	*line = NULL;
+
+	while (get_next_line(0, &line) > 0)
+	{
+		ptr = line;
+		value = get_value(&ptr);
+		i = -1;
+		while (++i < 11)
+		{
+			if (insn[i].value == value)
+			{
+				insn[i].func(&stack);
+				if (stop == 2)
+					stack_state(stack);
+				break ;
+			}
+		}
+		if (i == 11)
+			log_error(NOTINS);
+		ft_strdel(&line);
+	}
+	check_stack(stack);
+}
 
 int	main(int ac, char **av)
 {
-	static char			*line = NULL;
+	//sstatic char			*line = NULL;
 	char				*ptr;
 	t_push_stack		stack;
 	static t_instruc	insn[11] = {
@@ -62,10 +89,9 @@ int	main(int ac, char **av)
 		{0x7262, rotate_b}, {0x7272, rotate_r}, {0x727261, r_rotate_a}, \
 		{0x727262, r_rotate_b}, {0x727272, r_rotate_r}
 	};
-	int		value;
-	short	i;
-	static int	stop = 1;
+	static int			stop = 1;
 
+	ptr = NULL;
 	if (ac >= 2)
 	{
 		stack = new_push_stack();
@@ -81,26 +107,7 @@ int	main(int ac, char **av)
 			else
 				log_error(NONUM);
 		}
-		while (get_next_line(0, &line) > 0)
-		{
-			ptr = line;
-			value = get_value(&ptr);
-			i = -1;
-			while (++i < 11)
-			{
-				if (insn[i].value == value)
-				{
-					insn[i].func(&stack);
-					if (stop == 2)
-						stack_state(stack);
-					break ;
-				}
-			}
-			if (i == 11)
-				log_error(NOTINS);
-			ft_strdel(&line);
-		}
-		check_stack(stack);
+		checker(stack, ptr, insn, stop);
 	}
 	return (0);
 }
