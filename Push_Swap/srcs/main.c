d5fcf29b28fea9aaa987a9e65bb9f6bee16f6d61
akerdeka <kerdea@yahoo.fr>
2021-03-09 09:15:09 +0100

    starting push_swap

--- Push_Swap/srcs/main.c
+++ Push_Swap/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: akerdeka <akerdeka@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 13:13:05 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/08 14:07:23 by akerdeka         ###   ########lyon.fr   */
+/*   Updated: 2021/03/09 08:54:46 by akerdeka         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,34 +50,38 @@ void	check_stack(t_push_stack s)
 	exit(0);
 }
 
-static void	checker(t_push_stack s, char *ptr, t_instruc *insn, int stop)
+static void	push_swap(t_push_stack s, char *ptr, t_instruc *insn, int stop)
 {
-	int			value;
-	size_t		nb;
-	short		i;
-	static char	*line = NULL;
+	//int			value;
+	//size_t		nb;
+	//short		i;
+	//static char	*line = NULL;
 
-	nb = 0;
-	while (get_next_line(0, &line) > 0)
-	{
-		ptr = line;
-		value = get_value(&ptr);
-		i = -1;
-		while (++i < 11)
-		{
-			if (insn[i].value == value)
-			{
-				insn[i].func(&s);
-				nb++;
-				if (stop == 2)
-					stack_state(s, st_max(s.stack_a->_size, s.stack_b->_size), nb);
-				break ;
-			}
-		}
-		if (i == 11)
-			log_error(NOTINS);
-		ft_strdel(&line);
-	}
+	(void)ptr;
+	(void)insn;
+	(void)stop;
+
+	//nb = 0;
+	//while (get_next_line(0, &line) > 0)
+	//{
+	//	ptr = line;
+	//	value = get_value(&ptr);
+	//	i = -1;
+	//	while (++i < 11)
+	//	{
+	//		if (insn[i].value == value)
+	//		{
+	//			insn[i].func(&s);
+	//			nb++;
+	//			if (stop == 2)
+	//				stack_state(s, st_max(s.stack_a->_size, s.stack_b->_size), nb);
+	//			break ;
+	//		}
+	//	}
+	//	if (i == 11)
+	//		log_error(NOTINS);
+	//	ft_strdel(&line);
+	//}
 	check_stack(s);
 }
 
@@ -135,7 +139,7 @@ int	main(int ac, char **av)
 			while (stack.stack_b->_size)
 				insn[3].func(&stack);
 		}
-		checker(stack, ptr, insn, stop);
+		push_swap(stack, ptr, insn, stop);
 	}
 	return (0);
 }
