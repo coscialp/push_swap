a1528cb872ba603967ae0246b03b7a191b5cba7b
coscialp <corentin.scialpi@gmail.com>
2021-03-13 22:48:46 +0100

    parsing patch, handle double space and multi minus

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 10:49:50 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/11 10:50:20 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/13 22:45:25 by coscialp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@ void	parser(t_push_stack *s, t_instruc *insn, char **arg)
 {
 	char	*tok;
 
-	tok = NULL;
-	tok = ft_strsep(arg, " ");
-	while (tok)
+	while (1)
 	{
-		if (ft_stris(tok, ft_is_number))
+		tok = ft_strsep(arg, " ");
+		if (!tok)
+			break ;
+		if (!ft_strcmp(tok, ""))
+		{
+			tok = ft_strsep(arg, " ");
+			continue ;
+		}
+		if (ft_stris(tok[0] == '-' ? tok + 1 : tok, ft_isdigit))
 		{
 			if (s->stack_b->push(s->stack_b, ft_atoi(tok)) == -1 || \
 			no_duplicate_number(s->stack_a, ft_atoi(tok)))
@@ -29,7 +35,6 @@ void	parser(t_push_stack *s, t_instruc *insn, char **arg)
 		}
 		else
 			log_error(NONUM);
-		tok = ft_strsep(arg, " ");
 	}
 	while (s->stack_b->_size)
 		insn[3].func(s);
