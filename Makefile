372af8506a14addc16f1368be2f090786fdda4e8
coscialp <corentin.scialpi@gmail.com>
2021-03-31 16:00:32 +0200

    morm except algorythm

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ GREY = \033[3;90m
 ORANGE = \033[3;91m
 
 SRCS_NAME	=	new_push_stack.c push.c swap.c rotate.c r_rotate.c push_swap_utils.c result.c error.c get_value.c parser.c \
-				check_order.c find_median.c check_stack.c result_modifier.c
+				check_order.c find_median.c check_stack.c result_modifier.c find_smallest.c
 
 HEADER		=	Includes/
 
--- srcs/push_swap_utils.c
+++ srcs/push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 09:24:52 by akerdeka          #+#    #+#             */
-/*   Updated: 2021/03/31 10:32:35 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/31 13:55:00 by coscialp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,62 +90,4 @@ int	ft_log(size_t value)
 		ret++;
 	}
 	return (ret);
-}
-
-static int	find_smallest_element_a(t_push_stack stack)
-{
-	int				smallest;
-	t_node_stack	*tmp;
-
-	smallest = INT_MAX;
-	tmp = stack.stack_a->_data;
-	while (tmp)
-	{
-		if (tmp->value < smallest)
-			smallest = tmp->value;
-		tmp = tmp->_next;
-	}
-	return (smallest);
-}
-
-static int	find_smallest_element_b(t_push_stack stack)
-{
-	int				smallest;
-	t_node_stack	*tmp;
-
-	smallest = INT_MAX;
-	tmp = stack.stack_b->_data;
-	while (tmp)
-	{
-		if (tmp->value < smallest)
-			smallest = tmp->value;
-		tmp = tmp->_next;
-	}
-	return (smallest);
-}
-
-int	find_smallest_element(t_push_stack stack, int id_stack)
-{
-	if (id_stack == STACK_A)
-		return (find_smallest_element_a(stack));
-	else if (id_stack == STACK_B)
-		return (find_smallest_element_b(stack));
-	return (0);
-}
-
-int	find_smallest_element_index(t_push_stack stack, int smallest)
-{
-	t_node_stack	*tmp;
-	int				i;
-
-	tmp = stack.stack_a->_data;
-	i = 0;
-	while (tmp)
-	{
-		if (tmp->value == smallest)
-			break ;
-		tmp = tmp->_next;
-		i++;
-	}
-	return (i);
 }
